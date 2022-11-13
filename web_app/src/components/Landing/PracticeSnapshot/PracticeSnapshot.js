c68b491df94a2992ee2a432ed3ee08036e68db03
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-11-12 22:28:29 -0600

    sectionOne done

--- web_app/src/components/Landing/PracticeSnapshot/PracticeSnapshot.js
+++ web_app/src/components/Landing/PracticeSnapshot/PracticeSnapshot.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from './PracticeSnapshot.module.css';
 
 const PracticeSnapshot = (props) => {
-    console.log(props.currentSentence[0]);
+
+    let practiceText = null;
+    let translatedText = null;
+    if (props.currentSentence) {
+        practiceText = Object.keys(props.currentSentence)[0];
+        translatedText = props.currentSentence[Object.keys(props.currentSentence)[0]];
+    }
+
     return (
         <React.Fragment>
-            <div className={[classes.PracticeText, classes.Text].join(' ')}>
-                <p>夕食を作りましょうか</p>
+            <div className={classes.ArrowContainer} onClick={()=>props.clicked('left')}>
+                <i className={[classes.Arrow, classes.Left].join(' ')}></i>
             </div>
-            <div className={[classes.TranslatedText, classes.Text].join(' ')}>
-                <p>Shall we cook dinner</p>
+            <div className={classes.TextContainer}>
+                <div className={classes.PracticeText}>
+                    <p>{practiceText}</p>
+                </div>
+                <div className={classes.TranslatedText}>
+                    <p>{translatedText}</p>
+                </div>
+            </div>
+            <div className={classes.ArrowContainer} onClick={()=>props.clicked('right')}>
+                <i className={[classes.Arrow, classes.Right].join(' ')}></i>
             </div>
         </React.Fragment>
     );
--- web_app/src/components/Landing/SectionOne/SectionOne.js
+++ web_app/src/components/Landing/SectionOne/SectionOne.js
@@ -1,30 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import PracticeSnapshot from "../PracticeSnapshot/PracticeSnapshot";
 import classes from './SectionOne.module.css';
 
-const tempSentences = [
-    {"夕食を作りましょうか": "Shall we cook dinner"},
-    {"親友は何人いる？": "How many best friends do you have?"},
-    {"行くまいと決めた。": "I decided not to go."},
-]
-
-let curIndex = -1;
+let currIndex = -1;
 
 const SectionOne = () => {
-    const [sentences, setSentences] = useState('');
-    //change the practice text every few seconds
-    useEffect(() => {
-        const interval = setInterval(() => {
-            ++curIndex;
-            if (curIndex >= tempSentences.length) {
-                curIndex = 0;
-            }
-            setSentences(tempSentences[curIndex]);
-            console.log('This will run every 3 second!');
-        }, 3000);
-        return () => clearInterval(interval);
-    }, []);
+    const [loading, setLoading] = useState(true);
+    const [sentence, setSentence] = useState('');
+    const [tempSentences, setTempSentences] = useState([
+        {"夕食を作りましょうか": "Shall we cook dinner"},
+        {"親友は何人いる？": "How many best friends do you have?"},
+        {"行くまいと決めた。": "I decided not to go."},
+    ]);
 
+    const snapshotChangeHandler = (clickDirection) => {
+        displaySentenceHandler(clickDirection);
+    }
+
+    const displaySentenceHandler = useCallback((clicked) => {
+        
+        if (clicked) {
+            if (clicked === 'left') {
+                --currIndex;
+                console.log(currIndex);
+                if (currIndex < 0) {
+                    currIndex = tempSentences.length-1;
+                }
+                setSentence(tempSentences[currIndex]);
+            } else {
+                ++currIndex;
+                if (currIndex >= tempSentences.length) {
+                    currIndex = 0;
+                }
+                setSentence(tempSentences[currIndex]);
+            }
+        } else {
+            ++currIndex;
+            if (currIndex >= tempSentences.length) {
+                currIndex = 0;
+            }
+            setSentence(tempSentences[currIndex]);
+        }
+    }, [tempSentences]);
+
+    useEffect(() => {
+        setLoading(false);
+        displaySentenceHandler();
+        const interval = setInterval(() => {
+            displaySentenceHandler();
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [displaySentenceHandler]);
+
+
+    let snapshotContent = <p>Loading...</p>;
+    if (!loading) {
+        snapshotContent = (
+            <PracticeSnapshot currentSentence={sentence} clicked={snapshotChangeHandler} />
+        );
+    }
     return (
         <div className={classes.SectionOneContainer}>
             <div className={[classes.Box, classes.TextContainer].join(' ')}>
@@ -33,7 +67,7 @@ const SectionOne = () => {
                 <p className={classes.PracticeText}>Sentences</p>
             </div>
             <div className={[classes.Box, classes.SnapshotContainer].join(' ')}>
-                <PracticeSnapshot currentSentence={sentences} />
+                {snapshotContent}
             </div>
         </div>
     );
