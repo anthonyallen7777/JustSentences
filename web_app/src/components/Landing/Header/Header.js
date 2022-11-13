e92cb6ed34ec1828bb04355b092edc367ff5437d
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-11-12 18:27:33 -0600

    starting css

--- web_app/src/components/Landing/Header/Header.js
+++ web_app/src/components/Landing/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header>
+        <header className={classes.Header}>
             <h2>JstSentences</h2>
-            <NavLink to='/signin'>Sign In</NavLink>
+            <NavLink to='/signin' className={classes.SignInLink}>Sign In</NavLink>
         </header>
     );
 };
