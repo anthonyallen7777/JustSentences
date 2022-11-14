9f98fafe935791a2b18d23c3cfa73189a7526359
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-11-14 02:26:35 -0600

    languages css done

--- web_app/src/components/Base/Languages/Languages.js
+++ web_app/src/components/Base/Languages/Languages.js
@@ -2,27 +2,60 @@ import React from "react";
 import classes from './Languages.module.css';
 
 const Languages = (props) => {
+    const changePreferredLanguage = () => {
+        console.log("CHANGED");
+    }
+
     return (
-        <div>
-            <div>
+        <div className={classes.Container}>
+            <div className={classes.TitleContainer}>
                 <h2>Languages</h2>
             </div>
-            <div>
-                <div className={classes.Language}>
-                    <div>
-                        <form>
-                            <label htmlFor="language1">English</label>
-                            <div>
-                                <p>Preferred</p>
-                                <input type="checkbox" id="language1" name="english" value="Eng" />
-                            </div>
-                        </form>
-                        <div>
+            <div className={classes.LanguageMainContainer}>
+                <form className={classes.Form}>
+                    <div className={classes.FormHeaders}>
+                        <div className={classes.LanguagesHeader}>
+                            <p>Languages</p>
+                        </div>
+                        <div className={classes.PreferredHeader}>
+                            <p>Preferred</p>
+                        </div>
+                        <div className={classes.AmountPracticedHeader}>
                             <p>Amount Practiced</p>
-                            <p>5:32</p>
                         </div>
                     </div>
-                </div>
+                    <div className={classes.LanguagesContainer}>
+                        <div className={classes.IndividualLanguageContainer}>
+                            <div className={classes.Box}>
+                                <label htmlFor="language1">English</label>
+                            </div>
+                            <div className={classes.Box}>
+                                <input onChange={() => false} checked
+                                type="checkbox" id="language1" name="english" value="eng" />
+                            </div>
+                            <div className={classes.Box}>
+                                <div className={classes.AmountPracticedContainer}>
+                                    <p>5 Days, 4hrs, 32min</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.IndividualLanguageContainer}>
+                            <div className={classes.Box}>
+                                <label htmlFor="language2">Japanese</label>
+                            </div>
+                            <div className={classes.Box}>
+                                <input onChange={() => false}
+                                type="checkbox" id="language2" name="japanese" value="ja" />
+                            </div>
+                            <div className={classes.Box}>
+                                <div className={classes.AmountPracticedContainer}>
+                                    <p>0 Days, 1hrs, 32min</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                
             </div>
         </div>
     );
